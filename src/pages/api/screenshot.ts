ac4cb3e0f436138dfd349eb087ba36ef770e33f7
Huffer <hugoferreirait@gmail.com>
2024-01-18 18:29:03 -0300

    fix

--- src/pages/api/screenshot.ts
+++ src/pages/api/screenshot.ts
@@ -1,41 +1,46 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import chromium from 'chrome-aws-lambda';
 
-export const takeScreenshot = async (req: NextApiRequest, res: NextApiResponse) => {
-  const browser = await chromium.puppeteer.launch(
-    process.env.NODE_ENV === 'production'
-      ? {
-          args: chromium.args,
-          executablePath: await chromium.executablePath,
-          headless: chromium.headless,
-        }
-      : {}
-  );
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const browser = await chromium.puppeteer.launch(
+      process.env.NODE_ENV === 'production'
+        ? {
+            args: chromium.args,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+          }
+        : {}
+    );
 
-  const url = 'https://app.artemis.xyz/dashboard-builder/703';
+    const url = 'https://app.artemis.xyz/dashboard-builder/703';
 
-  console.log('resssss', url);
+    console.log('resssss', url);
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  // Change the URL as needed
-  await page.goto(url, { waitUntil: 'networkidle2' });
+    // Change the URL as needed
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
-  // Set the page view as needed
-  await page.setViewport({ width: 1920, height: 1080 });
+    // Set the page view as needed
+    await page.setViewport({ width: 1920, height: 1080 });
 
-  await page.waitForTimeout(1000);
+    await page.waitForTimeout(1000);
 
-  // Take the screenshot and save it to a file
-  const screenshot = await page.screenshot({ encoding: 'base64' });
+    // Take the screenshot and save it to a file
+    const screenshot = await page.screenshot({ encoding: 'base64' });
 
-  await browser.close();
+    await browser.close();
 
-  // Send the screenshot as a response
-  return res
-    .status(200)
-    .json({
+    // Send the screenshot as a response
+    return res.status(200).json({
       message: 'Screenshot taken and saved.',
       base64: 'data:image/png;base64,' + screenshot,
     });
-};
+  } else {
+    // Handle any other HTTP method
+  }
+}
