a9057052039201ad207ac4fbd9d3ba36e109e936
Huffer <hugoferreirait@gmail.com>
2024-01-19 10:26:50 -0300

    try playwright

--- src/pages/api/screenshot.ts
+++ src/pages/api/screenshot.ts
@@ -1,35 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import chromium from '@sparticuz/chromium';
-import puppeteer from 'puppeteer-core';
+import { chromium as playwright } from 'playwright-core';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    const browser = await puppeteer.launch(
-      process.env.NODE_ENV === 'production'
-        ? {
-            args: chromium.args,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
-          }
-        : {}
-    );
+    const browser = await playwright.launch({
+      executablePath:
+        process.env.NODE_ENV === 'production'
+          ? await chromium.executablePath
+          : undefined,
+      headless:
+        process.env.NODE_ENV === 'production' ? chromium.headless : true,
+    });
 
     const url = 'https://app.artemis.xyz/dashboard-builder/703';
 
-    const page = await browser.newPage();
+    const page = await browser.newPage({
+      viewport: { width: 1920, height: 1080 },
+    });
 
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    // Set the page view as needed
-    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(url, { waitUntil: 'networkidle' });
 
     await page.waitForTimeout(1000);
 
     // Take the screenshot
-    const screenshot = await page.screenshot({ encoding: 'base64' });
+    const buffer = await page.screenshot();
+
+    const screenshot = buffer.toString('base64');
 
     await browser.close();
 
