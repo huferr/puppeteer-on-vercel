b5c8d09a1f9c8fbb7bb58ddb81720ccb06545827
huferr <hugoferreirait@gmail.com>
2024-01-19 09:52:27 -0300

    Update screenshot.ts

--- src/pages/api/screenshot.ts
+++ src/pages/api/screenshot.ts
@@ -19,11 +19,8 @@ export default async function handler(
 
     const url = 'https://app.artemis.xyz/dashboard-builder/703';
 
-    console.log('resssss', url);
-
     const page = await browser.newPage();
 
-    // Change the URL as needed
     await page.goto(url, { waitUntil: 'networkidle2' });
 
     // Set the page view as needed
@@ -31,12 +28,11 @@ export default async function handler(
 
     await page.waitForTimeout(1000);
 
-    // Take the screenshot and save it to a file
+    // Take the screenshot
     const screenshot = await page.screenshot({ encoding: 'base64' });
 
     await browser.close();
 
-    // Send the screenshot as a response
     return res.status(200).json({
       message: 'Screenshot taken and saved.',
       base64: 'data:image/png;base64,' + screenshot,
